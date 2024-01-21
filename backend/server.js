1585c72fc9147daf3075ffa2a6a5904c00645289
kristzz <emils.krists@gmail.com>
2024-01-21 22:05:31 +0200

    pabeidzu

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const port = 5050;
 const express = require('express');
 const cors = require('cors');
@@ -42,6 +43,32 @@ app.post('/register', async (req, res) => {
     });
 });
 
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    
+    pool.query('SELECT * FROM users WHERE email = ?', [email], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('An error occurred while logging in.');
+            return;
+        }
+
+        if (results.length > 0) {
+            const user = results[0];
+            const validPassword = bcrypt.compareSync(password, user.pass);
+            if (validPassword) {
+                const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+                res.json({ auth: true, token: token });
+            }
+            else {
+                res.status(401).send('Invalid password!');
+            }
+        }
+        else {
+            res.status(404).send('User not found!');
+        }
+    });
+});
 
 app.get('/', (req, res) => {
     res.send('EJ NAHUJ!');
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,3 +1,41 @@
 <template>
-    <h1></h1>
+    <input id="email" type="email" v-model="email" placeholder="youremail@example.com">
+    <input id="password" type="password" v-model="password" placeholder="password">
+    <button @click.prevent="login">Login</button>
 </template>
+
+<script lang="ts">
+export default{
+    data() {
+        return{
+            email: '',
+            password: '',
+        };
+    },
+    methods:{
+        login(){
+            fetch('http://localhost:5050/login',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: this.email,
+                    password: this.password,
+                }),
+            }).then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error('Failed to login');
+                }
+            }).then((data) => {
+                localStorage.setItem('token', data.token);
+                this.$router.push({ name: 'success' });
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+    }
+}
+</script>
